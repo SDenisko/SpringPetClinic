81b29ff4efff1154391c554bb60f318e9f549fd4
SDenisko <93583549+SDenisko@users.noreply.github.com>
2022-02-07 14:12:19 +0200

    volume /tmp/

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -25,5 +25,4 @@ echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
 ls -al target/
-cp ./target/*.jar /tmp
 #java -jar target/${NAME}-${VERSION}.jar
